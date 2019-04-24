aaa65f931b052c72eb8bc007599c3c431606d5e4
alkoleek <36681691+alkoleek@users.noreply.github.com>
2019-04-25 00:03:27 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -70,7 +70,7 @@ function PDFInvoice(_ref) {
     genTableRow: function genTableRow() {
       items.map(function (item) {
         return Object.assign({}, item, {
-          amount: numeral(item.amount).format('0,00.00 euros')
+          amount: numeral(item.amount).format('0,00.00')
         });
       }).forEach(function (item, itemIndex) {
         ['amount', 'name', 'description', 'quantity'].forEach(function (field, i) {
